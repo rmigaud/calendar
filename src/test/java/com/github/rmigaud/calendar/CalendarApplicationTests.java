6485dda826a48c74b68f37f44d624e8542b29529
Ryan Migaud <rmigaud@asu.edu>
2023-02-22 22:48:04 -0800

    Initial commit, restful calendar api

---
+++ src/test/java/com/github/rmigaud/calendar/CalendarApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.rmigaud.calendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(2, 1 + 1);
+	}
+
+}
